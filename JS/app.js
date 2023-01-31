60c1fff7a891a9dd71b7d53444ee60ade3248c76
Nicolas Chaves <nicochaves92@gmail.com>
2023-01-30 23:50:19 -0300

    Editar cards

--- JS/app.js
+++ JS/app.js
@@ -19,49 +19,65 @@ const ropa = [
   {
     id: 1,
     name: 'Ropa Mujer',
-    Text: 'Cualquier texto Cualquier texto Cualquier texto Cualquier texto Cualquier texto Cualquier texto',
+    Text: 'REMERA ROCKYS 5',
+    TextDos: '3 cuotas de $2000',
+    precio: '$6000',
     img: "./Imagenes/card1.jpg",
   },
   {
     id: 2,
     name: 'Ropa Hombre',
-    Text: 'Cualquier texto Cualquier texto Cualquier texto Cualquier texto Cualquier texto Cualquier texto',
+    Text: 'REMERA ROCKYS 4',
+    TextDos: '3 cuotas de $2000',
+    precio: '$5000',
     img: "./Imagenes/card2.jpg",
   },
   {
     id: 3,
     name: 'Ropa Todo',
-    Text: 'Cualquier texto Cualquier texto Cualquier texto Cualquier texto Cualquier texto Cualquier texto',
+    Text: 'REMERA ROCKYS 3',
+    TextDos: '3 cuotas de $2000',
+    precio: '$3000',
     img: "./Imagenes/card3.jpg",
   },
   {
     id: 4,
     name: 'Ropa Mujer',
-    Text: 'Cualquier texto Cualquier texto Cualquier texto Cualquier texto Cualquier texto Cualquier texto',
+    Text: 'REMERA ROCKYS 3',
+    TextDos: '3 cuotas de $2000',
+    precio: '$5000',
     img: "./Imagenes/card1.jpg",
   },
   {
     id: 5,
     name: 'Ropa Mujer',
-    Text: 'Cualquier texto Cualquier texto Cualquier texto Cualquier texto Cualquier texto Cualquier texto',
+    Text: 'REMERA ROCKYS 2',
+    TextDos: '3 cuotas de $2000',
+    precio: '$6000',
     img: "./Imagenes/card1.jpg",
   },
   {
     id: 6,
     name: 'Ropa Hombre',
-    Text: 'Cualquier texto Cualquier texto Cualquier texto Cualquier texto Cualquier texto Cualquier texto',
+    Text: 'REMERA ROCKYS 8',
+    TextDos: '3 cuotas de $2000',
+    precio: '$4000',
     img: "./Imagenes/card2.jpg",
   },
   {
     id: 7,
     name: 'Ropa Todo',
-    Text: 'Cualquier texto Cualquier texto Cualquier texto Cualquier texto Cualquier texto Cualquier texto',
+    Text: 'REMERA ROCKYS 9',
+    TextDos: '3 cuotas de $2000',
+    precio: '$6000',
     img: "./Imagenes/card3.jpg",
   },
   {
     id: 8,
     name: 'Ropa Mujer',
-    Text: 'Cualquier texto Cualquier texto Cualquier texto Cualquier texto Cualquier texto Cualquier texto',
+    Text: 'REMERA ROCKYS 10',
+    TextDos: '3 cuotas de $2000',
+    precio: '$6000',
     img: "./Imagenes/card1.jpg",
   },
 ];
@@ -73,8 +89,9 @@ ropa.map ((x) => {
     <div class="card h-100">
       <img src="${x.img}" class="card-img-top" alt="${x.name}">
       <div class="card-body">
-        <h5 class="card-title">${x.name}</h5>
-        <p class="card-text">${x.Text}</p>
+        <h5 class="card-title">${x.Text}</h5>
+        <p class="card-text fw-semibold">Precio: ${x.precio}</p>
+        <p class="card-text fw-lighter">${x.TextDos}</p>
       </div>
     </div>
   `;
